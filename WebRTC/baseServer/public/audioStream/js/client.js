753515cfa434f6d80ba860de10cbe5935c0527aa
LiBoran <iqorange@yeah.net>
2020-08-06 23:17:55 +0800

    shoot audio Tracks

--- WebRTC/baseServer/public/audioStream/js/client.js
+++ WebRTC/baseServer/public/audioStream/js/client.js
@@ -1,19 +1,12 @@
 'use strict'
 
-var player = document.querySelector('video#player');
+var player = document.querySelector('audio#player');
 
 var audioSource = document.querySelector("select#audioSource");
 var audioOutput = document.querySelector("select#audiooutput");
 var videoSource = document.querySelector("select#videoSource");
 
 var filtersSeletc = document.querySelector("select#filter");
-
-var snapshoot = document.querySelector('button#snapshoot');
-var picture = document.querySelector("canvas#picture");
-picture.width = 640;
-picture.height = 360;
-
-var divConstraints = document.querySelector('div#constraints')
 
 function start() {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -23,16 +16,7 @@ function start() {
         var deviceID = videoSource.value;
         // 设备参数设置
         navigator.mediaDevices.getUserMedia({
-            video: {
-                width: 1280,
-                height: 720,
-                frameRate: {
-                    min: 30,
-                    max: 60
-                },
-                facingMode: 'enviroment', // user
-                deviceId: deviceID ? deviceID : undefined
-            },
+            video: false,
             audio: {
                 volume: 95,
                 sampleRate: 96000,
@@ -47,9 +31,6 @@ function start() {
         // 处理音视频流
         .then((stream) => {
             player.srcObject = stream;
-            var videoTrack = stream.getVideoTracks()[0];
-            var videoConstraints = videoTrack.getSettings();
-            divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
             return navigator.mediaDevices.enumerateDevices();
         })
         .then((deviceInfos) => {
@@ -79,9 +60,4 @@ videoSource.onchange = start;
 
 filtersSeletc.onchange = () => {
     player.className = filtersSeletc.value;
-}
-
-snapshoot.onclick = () => {
-    picture.className = filtersSeletc.value;
-    picture.getContext('2d').drawImage(player, 0, 0, picture.width, picture.height);
 }
