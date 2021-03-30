2e4fe5b35ecb581558c8bd63a0a329d13c430167
LiBoran <iqorange@yeah.net>
2021-03-30 14:38:15 +0800

    rec

--- WebRTC/baseServer/public/mediaStream/js/client.js
+++ WebRTC/baseServer/public/mediaStream/js/client.js
@@ -1,75 +1,106 @@
 'use strict'
 
-var player = document.querySelector('video#player');
+//devices
+var audioSource = document.querySelector('select#audioSource');
+var audioOutput = document.querySelector('select#audioOutput');
+var videoSource = document.querySelector('select#videoSource');
 
-var audioSource = document.querySelector("select#audioSource");
-var audioOutput = document.querySelector("select#audiooutput");
-var videoSource = document.querySelector("select#videoSource");
+//filter
+var filtersSelect = document.querySelector('select#filter');
 
-var filtersSeletc = document.querySelector("select#filter");
-
-var snapshoot = document.querySelector('button#snapshoot');
-var picture = document.querySelector("canvas#picture");
+//picture
+var snapshot = document.querySelector('button#snapshot');
+var picture = document.querySelector('canvas#picture');
 picture.width = 640;
-picture.height = 360;
+picture.height = 480;
 
-var divConstraints = document.querySelector('div#constraints')
+var videoplay = document.querySelector('video#player');
+//var audioplay = document.querySelector('audio#audioplayer');
+
+//div
+var divConstraints = document.querySelector('div#constraints');
+
+//record
+var recvideo = document.querySelector('video#recplayer');
+var btnRecord = document.querySelector('button#record');
+var btnPlay = document.querySelector('button#recplay');
+var btnDownload = document.querySelector('button#download');
+
+var buffer;
+var mediaRecorder;
+
+//socket.io
+var btnConnect = document.querySelector('button#connect');
+var btnLeave = document.querySelector('button#leave');
+var inputRoom = document.querySelector('input#room');
+
+var socket;
+var room;
+
+function gotDevices(deviceInfos) {
+
+    deviceInfos.forEach(function (deviceinfo) {
+
+        var option = document.createElement('option');
+        option.text = deviceinfo.label;
+        option.value = deviceinfo.deviceId;
+
+        if (deviceinfo.kind === 'audioinput') {
+            audioSource.appendChild(option);
+        } else if (deviceinfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option);
+        } else if (deviceinfo.kind === 'videoinput') {
+            videoSource.appendChild(option);
+        }
+    })
+}
+
+function gotMediaStream(stream) {
+
+    var videoTrack = stream.getVideoTracks()[0];
+    var videoConstraints = videoTrack.getSettings();
+
+    divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
+
+    window.stream = stream;
+    videoplay.srcObject = stream;
+
+    //audioplay.srcObject = stream;
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(err) {
+    console.log('getUserMedia error:', err);
+}
 
 function start() {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        console.log('getUserMedia is not supported~');
+
+    if (!navigator.mediaDevices ||
+        !navigator.mediaDevices.getUserMedia) {
+
+        console.log('getUserMedia is not supported!');
         return;
+
     } else {
-        var deviceID = videoSource.value;
-        // 设备参数设置
-        navigator.mediaDevices.getUserMedia({
+
+        var deviceId = videoSource.value;
+        var constraints = {
             video: {
-                width: 1280,
-                height: 720,
-                frameRate: {
-                    min: 30,
-                    max: 60
-                },
-                facingMode: 'enviroment', // user
-                deviceId: deviceID ? deviceID : undefined
+                width: 640,
+                height: 480,
+                frameRate: 15,
+                facingMode: 'enviroment',
+                deviceId: deviceId ? {
+                    exact: deviceId
+                } : undefined
             },
-            audio: {
-                volume: 95,
-                sampleRate: 96000,
-                sampleSize: 24,
-                echoCancellation: true,
-                noiseSuppression: true,
-                latency: 40,
-                channelCount: 2,
-            },
-            // deviceID groupID 设备切换
-        })
-        // 处理音视频流
-        .then((stream) => {
-            player.srcObject = stream;
-            var videoTrack = stream.getVideoTracks()[0];
-            var videoConstraints = videoTrack.getSettings();
-            divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
-            return navigator.mediaDevices.enumerateDevices();
-        })
-        .then((deviceInfos) => {
-            deviceInfos.forEach((deviceInfo) => {
-                console.log(deviceInfo);
-                var option = document.createElement('option');
-                option.text = deviceInfo.label;
-                option.value = deviceInfo.deviceId;
-                if (deviceInfo.kind === 'audioinput') {
-                    audioSource.appendChild(option);
-                } else if (deviceInfo.kind === 'audiooutput') {
-                    audioOutput.appendChild(option);
-                } else if (deviceInfo.kind === 'viceoinput') {
-                    videoSource.appendChild(option);
-                }
-            })
-        })
-        .catch((e) => {
-            console.log('getUserMedia error: ' + e);
-        });
+            audio: false
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(gotMediaStream)
+            .then(gotDevices)
+            .catch(handleError);
     }
 }
 
@@ -77,11 +108,123 @@ start();
 
 videoSource.onchange = start;
 
-filtersSeletc.onchange = () => {
-    player.className = filtersSeletc.value;
+filtersSelect.onchange = function () {
+    videoplay.className = filtersSelect.value;
 }
 
-snapshoot.onclick = () => {
-    picture.className = filtersSeletc.value;
-    picture.getContext('2d').drawImage(player, 0, 0, picture.width, picture.height);
+snapshot.onclick = function () {
+    picture.className = filtersSelect.value;
+    picture.getContext('2d').drawImage(videoplay, 0, 0, picture.width, picture.height);
+}
+
+function handleDataAvailable(e) {
+    if (e && e.data && e.data.size > 0) {
+        buffer.push(e.data);
+    }
+}
+
+function startRecord() {
+
+    buffer = [];
+
+    var options = {
+        mimeType: 'video/webm;codecs=vp8'
+    }
+
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error(`${options.mimeType} is not supported!`);
+        return;
+    }
+
+    try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+        console.error('Failed to create MediaRecorder:', e);
+        return;
+    }
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10);
+
+}
+
+function stopRecord() {
+    mediaRecorder.stop();
+}
+
+btnRecord.onclick = () => {
+
+    if (btnRecord.textContent === 'Start Record') {
+        startRecord();
+        btnRecord.textContent = 'Stop Record';
+        btnPlay.disabled = true;
+        btnDownload.disabled = true;
+    } else {
+
+        stopRecord();
+        btnRecord.textContent = 'Start Record';
+        btnPlay.disabled = false;
+        btnDownload.disabled = false;
+
+    }
+}
+
+btnPlay.onclick = () => {
+    var blob = new Blob(buffer, {
+        type: 'video/webm'
+    });
+    recvideo.src = window.URL.createObjectURL(blob);
+    recvideo.srcObject = null;
+    recvideo.controls = true;
+    recvideo.play();
+}
+
+btnDownload.onclick = () => {
+    var blob = new Blob(buffer, {
+        type: 'video/webm'
+    });
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+
+    a.href = url;
+    a.style.display = 'none';
+    a.download = 'aaa.webm';
+    a.click();
+}
+
+function connectServer() {
+
+    socket = io.connect();
+
+    btnLeave.disabled = false;
+    btnConnect.disabled = true;
+
+    ///////
+    socket.on('joined', function (room, id) {
+        console.log('The User(' + id + ') have joined into ' + room);
+    });
+
+    socket.on('leaved', function (room, id) {
+        console.log('The User(' + id + ') have leaved from ' + room);
+    });
+
+    //join room
+    room = inputRoom.value;
+    if (room !== '') {
+        socket.emit('join', room);
+    }
+
+}
+
+btnConnect.onclick = () => {
+    connectServer();
+}
+
+btnLeave.onclick = () => {
+    if (room !== '') {
+        socket.emit('leave', room);
+        btnLeave.disabled = true;
+        btnConnect.disabled = false;
+        socket.disconnect();
+    }
 }
---
+++ WebRTC/baseServer/server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var serveIndex = require('serve-index');
+
+var express = require('express');
+var app = express();
+
+//顺序不能换
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
+
+var options = {
+	key  : fs.readFileSync('./cert/localhost+2-key.key'),
+	cert : fs.readFileSync('./cert/localhost+2-key.pem')
+}
+
+var https_server = https.createServer(options, app);
+https_server.listen(443, '0.0.0.0');
+
+var http_server = http.createServer(app);
+http_server.listen(80, '0.0.0.0');
